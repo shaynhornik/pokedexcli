d5f27b85c45ca53cf829ba883649e809ce352c9a
Shayn Hornik <shaynhornik@gmail.com>
2026-08-25 19:35:44 -0400

    saving after creating internal dir and pokecache pakage, still no functions in cache package

---
+++ repl.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"strings"
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func startRepl(cfg *config) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		words := cleanInput(userInput)
+		if len(words) == 0 {
+			fmt.Print("Please enter a command\n")
+			continue
+		}
+		firstWord := words[0]
+		command, ok := cfg.commands[firstWord]
+		if !ok {
+			fmt.Print("Unknown command\n")
+		} else {
+			err := command.callback(cfg)
+			if err != nil {
+				fmt.Printf("Error: %v", err)
+			}
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	lText := strings.ToLower(text)
+	var slice []string
+	slice = strings.Fields(lText)
+	return slice
+}
---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{
+			input: " hello world ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input: "This, Is, A, Test, Of, All, Commas,",
+			expected: []string{"this,", "is,", "a,", "test,", "of,", "all,", "commas,"},
+		},
+	}
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Expected: %d\nActual: %d\n", len(c.expected), len(actual))
+			continue
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Expected : %s\nActual: %s\n", expectedWord, word)
+			}
+		}
+	}
+}
+
+
